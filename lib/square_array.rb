ffe2a3dbf482f748d3b1192f682e47e016ba15ae
Nethelbert <gilblasse@gmail.com>
2019-10-21 12:49:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_arry = []
   
-  while array.size > 0 do
+  while array.size > count do
     new_arry << array[count].to_i * array[count].to_i
     count+=1
 end
